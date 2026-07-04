8b415b0757217817ca3a7320922c1d187f8520f6
Naveen02627 <jangidnaveen321@gail.com>
2026-07-04 13:14:21 +0530

    Handle Cross Origen

--- NotificationProducer/src/main/java/com/NotificationAPI/NotificationProducer/Controller/SendNotification.java
+++ NotificationProducer/src/main/java/com/NotificationAPI/NotificationProducer/Controller/SendNotification.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/notification")
+@CrossOrigin(origins = "*")
 public class SendNotification {
 
     private final NotificationProducer notificationProducer;
